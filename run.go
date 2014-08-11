50f9b4eb15bf124b5afd957d0c04f461031760ff
Peng Gao <peng.gao.dut@gmail.com>
2014-08-12 01:04:21 +0800

    writefile

--- run.go
+++ run.go
@@ -1,9 +1,10 @@
 package util
 
 import (
-	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path"
 )
 
 func Pwd() string {
@@ -20,17 +21,30 @@ func Run(name string, arg ...string) ([]byte, error) {
 	cmd := exec.Command(name, arg...)
 	return cmd.CombinedOutput()
 }
-
-func WriteFile(path string, content []byte) error {
-	if IsExist(path) {
-		os.Remove(path)
-	}
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	n, err := f.Write(content)
-	fmt.Println("num", n)
+func CreateFile(filePath string) error {
+	os.MkdirAll(path.Dir(filePath), os.ModePerm)
+	_, err := os.Create(filePath)
 	return err
+}
+
+// SaveFile saves content type '[]byte' to file by given path.
+// It returns error when fail to finish operation.
+func WriteFile(filePath string, b []byte) (int, error) {
+	os.MkdirAll(path.Dir(filePath), os.ModePerm)
+	fw, err := os.Create(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer fw.Close()
+	return fw.Write(b)
+}
+
+func CopyFile(filePath string, reader io.Reader) (int64, error) {
+	os.MkdirAll(path.Dir(filePath), os.ModePerm)
+	fw, err := os.Create(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer fw.Close()
+	return io.Copy(fw, reader)
 }
