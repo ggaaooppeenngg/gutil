ce8087db9d1a91ab53c30b60b3733117135075ba
Peng Gao <peng.gao.dut@gmail.com>
2015-02-02 22:21:29 +0800

    check heap implementation

--- container/edge_priority_queue.go
+++ container/edge_priority_queue.go
@@ -12,55 +12,52 @@ type Item struct {
 
 //PQ is the priority queue containing *Vertex.
 
-type PQ []*Item
+type PQ struct {
+	items []*Item
+	index map[*Vertex]struct{}
+}
 
-func (pq PQ) Len() int { return len(pq) }
+func (pq PQ) Len() int { return len(pq.items) }
 func (pq PQ) Less(i, j int) bool {
-	if pq[i].weight == nil {
+	if pq.items[i].weight == nil {
 		return true
 	}
-	if pq[j].weight == nil {
+	if pq.items[j].weight == nil {
 		return false
 	}
-	if *(pq[i].weight) < *(pq[j].weight) {
+	if *(pq.items[i].weight) < *(pq.items[j].weight) {
 		return true
 	}
 	return false
 }
 func (pq PQ) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
+	pq.items[i], pq.items[j] = pq.items[j], pq.items[i]
 }
+
 func (pq *PQ) Push(x interface{}) {
-	n := len(*pq)
+	n := pq.Len()
 	item := new(Item)
 	item.edge = x.(*edge)
 	item.index = n
-	*pq = append(*pq, item)
-}
-func (pq *PQ) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	item.index = -1 //for safety
-	*pq = old[0 : n-1]
-	return item.edge
+	pq.items = append(pq.items, item)
 }
 
-//更改item.index的内容并且调整.
-func (pq *PQ) update(item *Item, e *edge) {
-	item.edge = e
-	heap.Fix(pq, item.index)
+//BUG:pop有问题
+func (pq *PQ) Pop() interface{} {
+	old := pq.items
+	if len(old) > 0 {
+		item := old[0]
+		old[0], old[len(old)-1] = old[len(old)-1], old[0]
+		item.index = -1 //for safety
+		*pq = old[0 : len(old)-1]
+		return item.edge
+	} else {
+
+	}
 }
 
 //含有某个结点.
-func (pq PQ) contains(v *Vertex) int {
-	index := -1
-	for _, item := range pq {
-		if item.vtx == v {
-			return item.index
-		}
-	}
-	return index
+func (pq PQ) contains(v *Vertex) bool {
+	_, ok := pq.index[v]
+	return ok
 }
